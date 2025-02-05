ba716a6e63c4e312307f478751bfcceb803c609a
akhilasingapuram <62023490+akhilasingapuram@users.noreply.github.com>
2025-02-05 22:05:22 +0530

    committed code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Location from "./Location";
+
+function App() {
+  return (
+    <div>
+      <Location />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Location.jsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+
+const Location = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("");
+  const [states, setStates] = useState([]);
+  const [state, setState] = useState("");
+  const [cities, setCities] = useState([]);
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const data = await fetch(
+          "https://crio-location-selector.onrender.com/countries"
+        );
+        const jsonData = await data.json();
+        setCountries(jsonData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCountries();
+  }, []);
+
+  useEffect(() => {
+    const fetchStates = async (countryName) => {
+      try {
+        const data = await fetch(
+          `https://crio-location-selector.onrender.com/country=${countryName}/states`
+        );
+        const jsonData = await data.json();
+        setStates(jsonData);
+        if (cities.length > 0) setCities([]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    if (country !== "") {
+      fetchStates(country);
+      if (state !== "") setState("");
+      if (city !== "") setCity("");
+    }
+  }, [country]);
+
+  useEffect(() => {
+    const fetchCities = async (stateName) => {
+      try {
+        const data = await fetch(
+          `https://crio-location-selector.onrender.com/country=${country}/state=${stateName}/cities`
+        );
+        const jsonData = await data.json();
+        setCities(jsonData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    if (state !== "") {
+      fetchCities(state);
+    }
+  }, [state]);
+
+  return (
+    <div>
+      <h1>Select Location</h1>
+      <br />
+      <select
+        placeholder="Select Country"
+        defaultValue={country}
+        onChange={(e) => setCountry(e.target.value)}
+      >
+        <option>Select Country</option>
+        {countries.map((country) => (
+          <option key={country}>{country}</option>
+        ))}
+      </select>
+
+      <select
+        placeholder="Select State"
+        defaultValue={state}
+        onChange={(e) => setState(e.target.value)}
+        disabled={country === ""}
+      >
+        <option>Select State</option>
+        {states.map((state) => (
+          <option key={state}>{state}</option>
+        ))}
+      </select>
+      <select
+        placeholder="Select City"
+        defaultValue={city}
+        onChange={(e) => setCity(e.target.value)}
+        disabled={state === ""}
+      >
+        <option>Select City</option>
+        {cities.map((city) => (
+          <option key={city}>{city}</option>
+        ))}
+      </select>
+      <br />
+      {city !== "" && state !== "" && country !== "" && (
+        <h1>
+          You selected {city},{state},{country}
+        </h1>
+      )}
+    </div>
+  );
+};
+export default Location;
